6d1a2b62e4d887bab34fb02ca19fdc522cc8640c
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-08-25 02:06:47 +0600

    file setup

---
+++ basic-es6-es5/default.js
@@ -0,0 +1,7 @@
+// default --> if value is not provided, take this as a default. 
+function add(num1, num2=0){
+    const result = num1 + num2;
+    console.log( num1, num2, result)
+    return result;
+}
+const sum = add(5,2);
